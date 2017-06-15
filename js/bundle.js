06dd893842f366d28c216e566a3bce63053643ff
Jakub Begera <jakub@begera.cz>
2017-06-15 17:23:20 +0200

    graph almost done

--- js/bundle.js
+++ js/bundle.js
@@ -72,36 +72,6 @@
 
 "use strict";
 /**
- * Represents node of DFA
- * Created by Jakub on 15.06.17.
- */
-
-const NODE_START = "S";
-/* harmony export (immutable) */ __webpack_exports__["a"] = NODE_START;
-
-const NODE_NORMAL = "N";
-/* harmony export (immutable) */ __webpack_exports__["d"] = NODE_NORMAL;
-
-const NODE_TERMINATE = "T";
-/* harmony export (immutable) */ __webpack_exports__["c"] = NODE_TERMINATE;
-
-
-class Node {
-
-    constructor(id, type) {
-        this.id = id;
-        this.type = type;
-    }
-}
-/* harmony export (immutable) */ __webpack_exports__["b"] = Node;
-
-
-/***/ }),
-/* 1 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/**
  * Represents edge of DFA
  * Created by Jakub on 15.06.17.
  */
@@ -118,42 +88,64 @@ class Edge {
 
 
 /***/ }),
+/* 1 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/**
+ * Represents node of DFA
+ * Created by Jakub on 15.06.17.
+ */
+
+const NODE_START = "S";
+/* harmony export (immutable) */ __webpack_exports__["a"] = NODE_START;
+
+const NODE_NORMAL = "N";
+/* harmony export (immutable) */ __webpack_exports__["b"] = NODE_NORMAL;
+
+const NODE_TERMINATE = "T";
+/* harmony export (immutable) */ __webpack_exports__["c"] = NODE_TERMINATE;
+
+
+class Node {
+
+    constructor(id, type) {
+        this.id = id;
+        this.type = type;
+    }
+}
+/* harmony export (immutable) */ __webpack_exports__["d"] = Node;
+
+
+/***/ }),
 /* 2 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__Node__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__Edge__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__Node__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__Edge__ = __webpack_require__(0);
 /**
  * Created by Jakub on 14.06.17.
  */
 
 
 
+let alphabet = ['0', '1'];
+let nodeId = 0;
+let edgeId = 0;
+let nodes = [];
+let edges = [];
 
-var alphabet = ['0', '1'];
-var nodeId = 0;
-var edgeId = 0;
-var nodes = [];
-var edges = [];
+let newEdgeNode1 = -1;
+let newEdgeNode2 = -1;
 
+//**********************************************************************************************************************
 // init graph
+//**********************************************************************************************************************
 var cy = cytoscape({
 
     container: $('#cy'), // container to render in
-
-    // elements: [ // list of graph elements to start with
-    //     { // node a
-    //         data: {id: 'a'}
-    //     },
-    //     { // node b
-    //         data: {id: 'b'}
-    //     },
-    //     { // edge ab
-    //         data: {id: 'ab', source: 'a', target: 'b', label: '0'}
-    //     }
-    // ],
 
     style: cytoscape.stylesheet()
         .selector('node').css({
@@ -173,8 +165,8 @@ var cy = cytoscape({
             'content': 'data(label)',
             'line-color': '#888',
             'target-arrow-color': '#888',
-            'text-outline-width' : 3,
-            'text-outline-color' : '#fff',
+            'text-outline-width': 3,
+            'text-outline-color': '#fff',
 
 
         }),
@@ -187,10 +179,26 @@ var cy = cytoscape({
     boxSelectionEnabled: true,
 });
 
+displayAlphabet();
 addNode(__WEBPACK_IMPORTED_MODULE_0__Node__["a" /* NODE_START */]);
+initNodeAdd();
+
+//**********************************************************************************************************************
+// node add handling
+//**********************************************************************************************************************
+document.querySelector("#btnAddNodeRegular").addEventListener('click', (e) => addNode(__WEBPACK_IMPORTED_MODULE_0__Node__["b" /* NODE_NORMAL */]));
+document.querySelector("#btnAddNodeTerminate").addEventListener('click', (e) => addNode(__WEBPACK_IMPORTED_MODULE_0__Node__["c" /* NODE_TERMINATE */]));
+document.querySelector("#btnClear").addEventListener('click', (e) => {
+    cy.elements().remove();
+    nodeId = 0;
+    edgeId = 0;
+    nodes = [];
+    edges = [];
+    addNode(__WEBPACK_IMPORTED_MODULE_0__Node__["a" /* NODE_START */]);
+});
 
 function addNode(type) {
-    let n = new __WEBPACK_IMPORTED_MODULE_0__Node__["b" /* default */](nodeId, type);
+    let n = new __WEBPACK_IMPORTED_MODULE_0__Node__["d" /* default */](nodeId, type);
     nodes.push(n);
     let color;
     switch (type) {
@@ -203,18 +211,20 @@ function addNode(type) {
         default:
             color = '#000';
     }
-    if (nodeId > 0) {
-        console.log(cy.getElementById(nodeId-1));
-    }
 
     cy.add({
         group: "nodes",
         data: {id: nodeId, label: "q" + nodeId, color: color},
         classes: 'center-center',
-        position: {x: 200, y: 200}
+        position: {x: 200 + 10 * nodeId, y: 200 + 10 * nodeId}
     });
     nodeId++;
 }
+
+//**********************************************************************************************************************
+// edge add handling
+//**********************************************************************************************************************
+document.querySelector("#btnDiscardAddingEdge").addEventListener('click', (e) => initNodeAdd());
 
 function addEdge(nodeFromId, nodeToId, transitionChar) {
     let e = new __WEBPACK_IMPORTED_MODULE_1__Edge__["a" /* default */](edgeId, nodeFromId, nodeToId, transitionChar);
@@ -226,11 +236,85 @@ function addEdge(nodeFromId, nodeToId, transitionChar) {
     nodeId++;
 }
 
+cy.on('tap', 'node', function (evt) {
+    let id = evt.target.id();
+    console.log("Clicked node: " + id);
+    if (newEdgeNode1 === -1) {
+        if (findAllPossibleNewEdgesFromNode(id).length === 0) {
+            $('#lblAddEdge').text("No free transition characters for node " + id + ".");
+            return;
+        }
+        newEdgeNode1 = id;
+        $('#lblAddEdge').text(id + " => click on destination node");
+        document.getElementById('btnDiscardAddingEdge').style.display = "inline";
+    } else if (newEdgeNode2 === -1) {
+        newEdgeNode2 = id;
+        document.getElementById('btnConfirmNewEdge').style.display = "inline";
+        document.getElementById('charSelect').style.display = "inline";
+        $('#lblAddEdge').text(newEdgeNode1 + " => " + newEdgeNode2 + " Transition character:");
+        let possibleChars = findAllPossibleNewEdgesFromNode(newEdgeNode1);
+        console.log("Possible chars from node " + newEdgeNode1 + ": " + possibleChars);
+        let select = $('#charSelect');//document.getElementById('#charSelect');
+        select.find('option').remove();
+        for (let i = 0; i < possibleChars.length; i++) {
+            let opt = document.createElement('option');
+            opt.value = possibleChars[i];
+            opt.innerHTML = possibleChars[i];
+            select.append(opt);
+        }
+    }
+});
+
+function initNodeAdd() {
+    document.getElementById('btnDiscardAddingEdge').style.display = "none";
+    document.getElementById('btnConfirmNewEdge').style.display = "none";
+    document.getElementById('charSelect').style.display = "none";
+    $('#lblAddEdge').text("Click on starting edge");
+    newEdgeNode1 = -1;
+    newEdgeNode2 = -1;
+}
+
+document.querySelector("#btnConfirmNewEdge").addEventListener('click', (e) => {
+    let selected = $('#charSelect').find(":selected").text();
+    console.log(selected);
+    addEdge(newEdgeNode1, newEdgeNode2, selected);
+    initNodeAdd();
+});
+
+function findAllPossibleNewEdgesFromNode(nodeId) {
+    let out = alphabet.slice();
+    edges.forEach(function (edge) {
+        if (edge.nodeFrom === nodeId) {
+            // remove it from alphabet
+            for (let i = out.length - 1; i >= 0; i--) {
+                if (out[i] === edge.transitionChar) {
+                    out.splice(i, 1);
+                }
+            }
+        }
+    });
+    return out;
+}
+
+//**********************************************************************************************************************
+// others
+//**********************************************************************************************************************
+function displayAlphabet() {
+    let alp = "";
+    for (let i = 0; i < alphabet.length; i++) {
+        if (i !== 0) {
+            alp = alp + ", ";
+        }
+        alp = alp + alphabet[i];
+        console.log(alp);
+    }
+    $('#automataAlphabet').text(alp);
+}
 
 $(window).keypress(function (e) {
     if (e.keyCode === 0 || e.keyCode === 32) { // space
-        addNode(__WEBPACK_IMPORTED_MODULE_0__Node__["d" /* NODE_NORMAL */]);
-        addEdge(nodes[nodes.length- 2].id, nodes[nodes.length - 1].id, '1');
+        addNode(__WEBPACK_IMPORTED_MODULE_0__Node__["b" /* NODE_NORMAL */]);
+        addEdge(nodes[nodes.length - 2].id, nodes[nodes.length - 1].id, '1');
     }
 });
 
--- src/main.js
+++ src/main.js
@@ -1,36 +1,24 @@
 /**
  * Created by Jakub on 14.06.17.
  */
-import {
-    NODE_START,
-    NODE_NORMAL,
-    NODE_TERMINATE
-} from './Node';
-import Node from './Node';
-import Edge from './Edge';
+import Node, {NODE_NORMAL, NODE_START, NODE_TERMINATE} from "./Node";
+import Edge from "./Edge";
 
-var alphabet = ['0', '1'];
-var nodeId = 0;
-var edgeId = 0;
-var nodes = [];
-var edges = [];
+let alphabet = ['0', '1'];
+let nodeId = 0;
+let edgeId = 0;
+let nodes = [];
+let edges = [];
 
+let newEdgeNode1 = -1;
+let newEdgeNode2 = -1;
+
+//**********************************************************************************************************************
 // init graph
+//**********************************************************************************************************************
 var cy = cytoscape({
 
     container: $('#cy'), // container to render in
-
-    // elements: [ // list of graph elements to start with
-    //     { // node a
-    //         data: {id: 'a'}
-    //     },
-    //     { // node b
-    //         data: {id: 'b'}
-    //     },
-    //     { // edge ab
-    //         data: {id: 'ab', source: 'a', target: 'b', label: '0'}
-    //     }
-    // ],
 
     style: cytoscape.stylesheet()
         .selector('node').css({
@@ -50,8 +38,8 @@ var cy = cytoscape({
             'content': 'data(label)',
             'line-color': '#888',
             'target-arrow-color': '#888',
-            'text-outline-width' : 3,
-            'text-outline-color' : '#fff',
+            'text-outline-width': 3,
+            'text-outline-color': '#fff',
 
 
         }),
@@ -64,7 +52,23 @@ var cy = cytoscape({
     boxSelectionEnabled: true,
 });
 
+displayAlphabet();
 addNode(NODE_START);
+initNodeAdd();
+
+//**********************************************************************************************************************
+// node add handling
+//**********************************************************************************************************************
+document.querySelector("#btnAddNodeRegular").addEventListener('click', (e) => addNode(NODE_NORMAL));
+document.querySelector("#btnAddNodeTerminate").addEventListener('click', (e) => addNode(NODE_TERMINATE));
+document.querySelector("#btnClear").addEventListener('click', (e) => {
+    cy.elements().remove();
+    nodeId = 0;
+    edgeId = 0;
+    nodes = [];
+    edges = [];
+    addNode(NODE_START);
+});
 
 function addNode(type) {
     let n = new Node(nodeId, type);
@@ -85,10 +89,15 @@ function addNode(type) {
         group: "nodes",
         data: {id: nodeId, label: "q" + nodeId, color: color},
         classes: 'center-center',
-        position: {x: 200, y: 200}
+        position: {x: 200 + 10 * nodeId, y: 200 + 10 * nodeId}
     });
     nodeId++;
 }
+
+//**********************************************************************************************************************
+// edge add handling
+//**********************************************************************************************************************
+document.querySelector("#btnDiscardAddingEdge").addEventListener('click', (e) => initNodeAdd());
 
 function addEdge(nodeFromId, nodeToId, transitionChar) {
     let e = new Edge(edgeId, nodeFromId, nodeToId, transitionChar);
@@ -100,10 +109,84 @@ function addEdge(nodeFromId, nodeToId, transitionChar) {
     nodeId++;
 }
 
+cy.on('tap', 'node', function (evt) {
+    let id = evt.target.id();
+    console.log("Clicked node: " + id);
+    if (newEdgeNode1 === -1) {
+        if (findAllPossibleNewEdgesFromNode(id).length === 0) {
+            $('#lblAddEdge').text("No free transition characters for node " + id + ".");
+            return;
+        }
+        newEdgeNode1 = id;
+        $('#lblAddEdge').text(id + " => click on destination node");
+        document.getElementById('btnDiscardAddingEdge').style.display = "inline";
+    } else if (newEdgeNode2 === -1) {
+        newEdgeNode2 = id;
+        document.getElementById('btnConfirmNewEdge').style.display = "inline";
+        document.getElementById('charSelect').style.display = "inline";
+        $('#lblAddEdge').text(newEdgeNode1 + " => " + newEdgeNode2 + " Transition character:");
+        let possibleChars = findAllPossibleNewEdgesFromNode(newEdgeNode1);
+        console.log("Possible chars from node " + newEdgeNode1 + ": " + possibleChars);
+        let select = $('#charSelect');//document.getElementById('#charSelect');
+        select.find('option').remove();
+        for (let i = 0; i < possibleChars.length; i++) {
+            let opt = document.createElement('option');
+            opt.value = possibleChars[i];
+            opt.innerHTML = possibleChars[i];
+            select.append(opt);
+        }
+    }
+});
+
+function initNodeAdd() {
+    document.getElementById('btnDiscardAddingEdge').style.display = "none";
+    document.getElementById('btnConfirmNewEdge').style.display = "none";
+    document.getElementById('charSelect').style.display = "none";
+    $('#lblAddEdge').text("Click on starting edge");
+    newEdgeNode1 = -1;
+    newEdgeNode2 = -1;
+}
+
+document.querySelector("#btnConfirmNewEdge").addEventListener('click', (e) => {
+    let selected = $('#charSelect').find(":selected").text();
+    console.log(selected);
+    addEdge(newEdgeNode1, newEdgeNode2, selected);
+    initNodeAdd();
+});
+
+function findAllPossibleNewEdgesFromNode(nodeId) {
+    let out = alphabet.slice();
+    edges.forEach(function (edge) {
+        if (edge.nodeFrom === nodeId) {
+            // remove it from alphabet
+            for (let i = out.length - 1; i >= 0; i--) {
+                if (out[i] === edge.transitionChar) {
+                    out.splice(i, 1);
+                }
+            }
+        }
+    });
+    return out;
+}
+
+//**********************************************************************************************************************
+// others
+//**********************************************************************************************************************
+function displayAlphabet() {
+    let alp = "";
+    for (let i = 0; i < alphabet.length; i++) {
+        if (i !== 0) {
+            alp = alp + ", ";
+        }
+        alp = alp + alphabet[i];
+        console.log(alp);
+    }
+    $('#automataAlphabet').text(alp);
+}
 
 $(window).keypress(function (e) {
     if (e.keyCode === 0 || e.keyCode === 32) { // space
         addNode(NODE_NORMAL);
-        addEdge(nodes[nodes.length- 2].id, nodes[nodes.length - 1].id, '1');
+        addEdge(nodes[nodes.length - 2].id, nodes[nodes.length - 1].id, '1');
     }
 });
